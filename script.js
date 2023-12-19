bc8a8d4279e5ad9154d12376f163cbce535107b0
a-boyadzhiev <150182926+a-boyadzhiev@users.noreply.github.com>
2023-12-19 13:44:14 +0200

    Add a function that creates the grid

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('.container');
+
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -6,3 +8,27 @@ function getRandomColor() {
     }
     return color;
   }
+
+  function createGrid(size) {
+    removeGrid();
+    const boxSize = 100 / size; // Adjust this value based on your container size
+  
+    for (let i = 0; i < size * size; i++) {
+      let smallBox = document.createElement('div');
+      smallBox.className = 'small-box';
+      smallBox.style.width = `${boxSize}%`;
+      smallBox.style.height = `${boxSize}%`;
+      container.appendChild(smallBox);
+  
+      const reset = document.querySelector('#reset');
+      reset.addEventListener('click', () => {
+        smallBox.style.backgroundColor = 'white';
+      });
+  
+      smallBox.addEventListener('mouseover', () => {
+        smallBox.style.backgroundColor = getRandomColor();
+      });
+    }
+  }
+  
+  createGrid(16);
